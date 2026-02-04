f6a110b1888c4579775bdd337caa47a02294d10a
Sajal <sajalhaldar2004@gmail.com>
2026-02-04 23:13:43 +0530

    Initial Commit

--- train3times_seeds_5p.sh
+++ train3times_seeds_5p.sh
@@ -13,7 +13,7 @@ while getopts 'm:e:c:t:l:w:' OPT; do
 done
 echo $method
 echo $cuda
-  
+
 epoch=500
 echo $epoch
 
@@ -24,25 +24,13 @@ cps="AB"
 
 echo $folder
 
-# FOLD 1 - Seed 0
-echo "=== Starting Fold 1 (Seed 0) ==="
-python3 code/train_${method}.py --task ${task} --exp ${folder}${method}${exp}/fold1 --seed 0 -g ${cuda} --base_lr ${lr} -w ${cps_w} -ep ${epoch} -sl ${labeled_data} -su ${unlabeled_data} --alpha_noise 0.7 --min_alpha 0.05 --grad_clip 1.0 --warmup_epochs 50 --val_freq 20 --noise_convergence_thresh 0.01 --noise_safe_mode -r
+python3 code/train_${method}.py --task ${task} --exp ${folder}${method}${exp}/fold1 --seed 0 -g ${cuda} --base_lr ${lr} -w ${cps_w} -ep ${epoch} -sl ${labeled_data} -su ${unlabeled_data} -r
 python3 code/test.py --task ${task} --exp ${folder}${method}${exp}/fold1 -g ${cuda} --cps ${cps}
 python3 code/evaluate_Ntimes.py --task ${task} --exp ${folder}${method}${exp} --folds 1 --cps ${cps}
+#python3 code/train_${method}.py --exp ${folder}${method}${exp}/fold2 --seed 1 -g ${cuda} --base_lr ${lr} -w ${cps_w} -ep ${epoch} -sl ${labeled_data} -su ${unlabeled_data} -r
+#python3 code/test.py --exp ${folder}${method}${exp}/fold2 -g ${cuda} --cps ${cps}
+#python3 code/evaluate_Ntimes.py --exp ${folder}${method}${exp} --folds 2 --cps ${cps}
+#python3 code/train_${method}.py --exp ${folder}${method}${exp}/fold3 --seed 666 -g ${cuda} --base_lr ${lr} -w ${cps_w} -ep ${epoch} -sl ${labeled_data} -su ${unlabeled_data} -r
+#python3 code/test.py --exp ${folder}${method}${exp}/fold3 -g ${cuda} --cps ${cps}
 
-# FOLD 2 - Seed 1
-# echo "=== Starting Fold 2 (Seed 1) ==="
-# python3 code/train_${method}.py --task ${task} --exp ${folder}${method}${exp}/fold2 --seed 1 -g ${cuda} --base_lr ${lr} -w ${cps_w} -ep ${epoch} -sl ${labeled_data} -su ${unlabeled_data} --alpha_noise 0.7 --min_alpha 0.05 --grad_clip 1.0 --warmup_epochs 50 --val_freq 20 --noise_convergence_thresh 0.01 --noise_safe_mode -r
-# python3 code/test.py --task ${task} --exp ${folder}${method}${exp}/fold2 -g ${cuda} --cps ${cps}
-# python3 code/evaluate_Ntimes.py --task ${task} --exp ${folder}${method}${exp} --folds 2 --cps ${cps}
-
-# FOLD 3 - Seed 666
-# echo "=== Starting Fold 3 (Seed 666) ==="
-# python3 code/train_${method}.py --task ${task} --exp ${folder}${method}${exp}/fold3 --seed 666 -g ${cuda} --base_lr ${lr} -w ${cps_w} -ep ${epoch} -sl ${labeled_data} -su ${unlabeled_data} --alpha_noise 0.7 --min_alpha 0.05 --grad_clip 1.0 --warmup_epochs 50 --val_freq 20 --noise_convergence_thresh 0.01 --noise_safe_mode -r
-# python3 code/test.py --task ${task} --exp ${folder}${method}${exp}/fold3 -g ${cuda} --cps ${cps}
-
-# FINAL EVALUATION ALL FOLDS
-# echo "=== Final Evaluation All 3 Folds ==="
-# python3 code/evaluate_Ntimes.py --task ${task} --exp ${folder}${method}${exp} --folds 3 --cps ${cps}
-
-# echo "=== All 3 folds completed ==="
+#python3 code/evaluate_Ntimes.py --exp ${folder}${method}${exp} --cps ${cps}
